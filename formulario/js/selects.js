073e721fbe7f5d7895328e2d5196dc047e69b8b1
Jorge Leonardo Rodriguez <jrodriguez@organizacioncardenas.com>
2025-11-24 16:43:29 -0500

    agregear a ronal al selelect

--- formulario/js/selects.js
+++ formulario/js/selects.js
@@ -20,8 +20,8 @@ const datosPlantas = {
       "secador de aire 1",
       "secador de aire 2",
     ],
-    "Área Enfardadoras ": 
-       [
+    "Área Enfardadoras ":
+      [
         "Enfardadora 1",
         "Enfardadora 2",
         "Banda de salida Máquina 1",
@@ -34,15 +34,15 @@ const datosPlantas = {
         "Banda interconexión Enfardadora 2",
         "Banda rápida linea 1",
         "Banda rápida linea 2"
-      ],    
-
-      "Codificadoras": [
-        "tedmaq codificadora ", 
-        "tecnopack codificadora",
-        "inna 1 codificadora",
-        "inna 2 codificadora",
-        "rosdan codificadora"
       ],
+
+    "Codificadoras": [
+      "tedmaq codificadora ",
+      "tecnopack codificadora",
+      "inna 1 codificadora",
+      "inna 2 codificadora",
+      "rosdan codificadora"
+    ],
   },
   ASEO: {
     "Área de Disco": [
@@ -123,7 +123,7 @@ const datosPlantas = {
     "Área de Nitrógeno": ["Tanque de Nitrógeno", "PETAR"],
   },
 
-   OFERTAS: { 
+  OFERTAS: {
     "Área de Producción": [
       "Horno 1",
       "Horno 2",
@@ -134,93 +134,96 @@ const datosPlantas = {
     ]
   },
 
-   LOCATIVOS: { 
-    
-      "Bodega 14B": ["Planta 1", "Planta 2", "Planta 3"],
-      "Bodega 15B": ["Planta 1", "Planta 2", "Planta 3"],
-      "Bodega 17B": ["Planta 1", "Planta 2", "Planta 3"],
-      "Bodega 18B": ["Planta 1", "Planta 2", "Planta 3"],
-      "Bodega 8C":  ["Planta 1", "Planta 2", "Planta 3"]
-    
+  LOCATIVOS: {
+
+    "Bodega 14B": ["Planta 1", "Planta 2", "Planta 3"],
+    "Bodega 15B": ["Planta 1", "Planta 2", "Planta 3"],
+    "Bodega 17B": ["Planta 1", "Planta 2", "Planta 3"],
+    "Bodega 18B": ["Planta 1", "Planta 2", "Planta 3"],
+    "Bodega 19B": ["Planta 1", "Planta 2", "Planta 3"],
+    "Bodega 8A": ["Planta 1", "Planta 2", "Planta 3"],
+    "Bodega 8C": ["Planta 1", "Planta 2", "Planta 3"],
+    "Bodega 5C": ["Planta 1", "Planta 2", "Planta 3"]
+
   }
 };
 
 // Supervisores por planta
 const supervisores = {
-    ALIMENTOS: "PINZON GUEVARA WILLIAM ORLANDO",
-    ASEO: "RUEDA TOVAR JORGE CRISTIAN",
-    GRANOS: "LEAL HERRERA JORGE ANDRES",
-    BEBIDAS: "HENAO BEDOYA MARIA CRISTINA",
-    OFERTAS: "HENAO BEDOYA MARIA CRISTINA",
-    LOCATIVOS: "RICARDO ANDRES BEJARANO"
+  ALIMENTOS: "PINZON GUEVARA WILLIAM ORLANDO",
+  ASEO: "RUEDA TOVAR JORGE CRISTIAN",
+  GRANOS: "LEAL HERRERA JORGE ANDRES",
+  BEBIDAS: "HENAO BEDOYA MARIA CRISTINA",
+  OFERTAS: "HENAO BEDOYA MARIA CRISTINA",
+  LOCATIVOS: "RICARDO ANDRES BEJARANO"
 };
 
 // ------------------------------------------------------
 // FUNCIÓN PRINCIPAL DE INICIALIZACIÓN
 // ------------------------------------------------------
 function initSelects() {
-    const plantaSelect = document.getElementById("planta");
-    const areaSelect = document.getElementById("area");
-    const equipoSelect = document.getElementById("equipo");
+  const plantaSelect = document.getElementById("planta");
+  const areaSelect = document.getElementById("area");
+  const equipoSelect = document.getElementById("equipo");
 
-    if (!plantaSelect || !areaSelect || !equipoSelect) return; // evita errores si no existen
+  if (!plantaSelect || !areaSelect || !equipoSelect) return; // evita errores si no existen
 
-    // Cambiar fondo según planta seleccionada
-    function cambiarFondo() {
-        const contenedor = document.getElementById("contenedorPrincipal");
-        const fondos = {
-            GRANOS:
-                "url('https://mre-site-makro-colombia-test-webapp-slot.azurewebsites.net/imagesProducts/medias/913263_1_239.webp')",
-            ASEO: "url('https://organizacioncardenas.com/wp-content/uploads/2022/11/detergentelavado-e1678672244584-793x1024.png')",
-            ALIMENTOS:
-                "url('img/BUNUELO.png')",
-            BEBIDAS: "url('https://media.surtiplaza.co/dimen/7707335286316.png')",
+  // Cambiar fondo según planta seleccionada
+  function cambiarFondo() {
+    const contenedor = document.getElementById("contenedorPrincipal");
+    const fondos = {
+      GRANOS:
+        "url('https://mre-site-makro-colombia-test-webapp-slot.azurewebsites.net/imagesProducts/medias/913263_1_239.webp')",
+      ASEO: "url('https://organizacioncardenas.com/wp-content/uploads/2022/11/detergentelavado-e1678672244584-793x1024.png')",
+      ALIMENTOS:
+        "url('img/BUNUELO.png')",
+      BEBIDAS: "url('https://media.surtiplaza.co/dimen/7707335286316.png')",
 
-            OFERTAS:
-                "url('https://plus.unsplash.com/premium_photo-1681426730828-bfee2d13861d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1332')",
-            LOCATIVOS:
-                "url('https://images.unsplash.com/photo-1676311396794-f14881e9daaa?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170')",
-        };
-        contenedor.style.background = fondos[plantaSelect.value] || "#f6f9fc";
-    }
+      OFERTAS:
+        "url('https://plus.unsplash.com/premium_photo-1681426730828-bfee2d13861d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1332')",
+      LOCATIVOS:
+        "url('https://images.unsplash.com/photo-1676311396794-f14881e9daaa?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170')",
+    };
+    contenedor.style.background = fondos[plantaSelect.value] || "#f6f9fc";
+  }
 
-    // Mostrar supervisor
-    function mostrarSupervisor(planta) {
-        const supervisor = supervisores[planta.toUpperCase()] || "No asignado";
-        const label = document.getElementById("supervisor");
-        if (label) label.textContent = `👤 Supervisor: ${supervisor}`;
-    }
+  // Mostrar supervisor
+  function mostrarSupervisor(planta) {
+    const supervisor = supervisores[planta.toUpperCase()] || "No asignado";
+    const label = document.getElementById("supervisor");
+    if (label) label.textContent = `👤 Supervisor: ${supervisor}`;
+  }
 
-    // Evento: cambio de planta
-    plantaSelect.addEventListener("change", () => {
-        const planta = plantaSelect.value;
-        const areas = Object.keys(datosPlantas[planta] || {});
-        areaSelect.innerHTML =
-            "<option disabled selected>Seleccione un área</option>";
-        equipoSelect.innerHTML =
-            "<option disabled selected>Seleccione un equipo</option>";
-        areas.forEach((a) => {
-            const opt = document.createElement("option");
-            opt.value = a;
-            opt.textContent = a;
-            areaSelect.appendChild(opt);
-        });
-        cambiarFondo();
-        mostrarSupervisor(planta);
+  // Evento: cambio de planta
+  plantaSelect.addEventListener("change", () => {
+    const planta = plantaSelect.value;
+    const areas = Object.keys(datosPlantas[planta] || {});
+    areaSelect.innerHTML =
+      "<option disabled selected>Seleccione un área</option>";
+    equipoSelect.innerHTML =
+      "<option disabled selected>Seleccione un equipo</option>";
+    areas.forEach((a) => {
+      const opt = document.createElement("option");
+      opt.value = a;
+      opt.textContent = a;
+      areaSelect.appendChild(opt);
     });
+    cambiarFondo();
+    mostrarSupervisor(planta);
+  });
 
-    // Evento: cambio de área
-    areaSelect.addEventListener("change", () => {
-        const equipos = datosPlantas[plantaSelect.value]?.[areaSelect.value] || [];
-        equipoSelect.innerHTML =
-            "<option disabled selected>Seleccione un equipo</option>";
-        equipos.forEach((eq) => {
-            const opt = document.createElement("option");
-            opt.value = eq;
-            opt.textContent = eq;
-            equipoSelect.appendChild(opt);
-        });
+  // Evento: cambio de área
+  areaSelect.addEventListener("change", () => {
+    const equipos = datosPlantas[plantaSelect.value]?.[areaSelect.value] || [];
+    equipoSelect.innerHTML =
+      "<option disabled selected>Seleccione un equipo</option>";
+    equipos.forEach((eq) => {
+      const opt = document.createElement("option");
+      opt.value = eq;
+      opt.textContent = eq;
+      equipoSelect.appendChild(opt);
     });
+  });
 }
 
 // ------------------------------------------------------
